6afbe3e4ba67b3eee42749e70f69f59b805d1bc8
serxka <serxka@protonmail.com>
2022-01-31 17:20:50 +1100

    Ported to ti-nspire cx using ndless

--- makefile
+++ makefile
@@ -1,18 +1,48 @@
-OBJS=voxelspace.o
-CFLAGS=-O2
+DEBUG = FALSE
 
-all: voxelspace
+GCC = nspire-gcc
+AS  = nspire-as
+GXX = nspire-g++
+LD  = nspire-ld
+GENZEHN = genzehn
 
-voxelspace: stb_image.h ${OBJS}
-	${CC} ${CFLAGS} -lSDL2 -lm -o $@ ${OBJS}
-	strip $@
+GCCFLAGS = -Wall -W -marm
+LDFLAGS =
+ZEHNFLAGS = --name "voxelspace" 
 
-stb_image.h:
-	@echo downloading "stb_image.h" from source
-	curl https://raw.githubusercontent.com/nothings/stb/master/stb_image.h > stb_image.h
+ifeq ($(DEBUG),FALSE)
+	GCCFLAGS += -Os
+else
+	GCCFLAGS += -O0 -g
+endif
+
+OBJS = $(patsubst %.c, %.o, $(shell find . -name \*.c))
+OBJS += $(patsubst %.cpp, %.o, $(shell find . -name \*.cpp))
+OBJS += $(patsubst %.S, %.o, $(shell find . -name \*.S))
+EXE = voxelspace
+DISTDIR = .
+vpath %.tns $(DISTDIR)
+vpath %.elf $(DISTDIR)
+
+all: $(EXE).tns
+
+%.o: %.c
+	$(GCC) $(GCCFLAGS) -c $< -o $@
+
+%.o: %.cpp
+	$(GXX) $(GCCFLAGS) -c $< -o $@
+	
+%.o: %.S
+	$(AS) -c $< -o $@
+
+$(EXE).elf: $(OBJS)
+	mkdir -p $(DISTDIR)
+	$(LD) $^ -o $@ $(LDFLAGS)
+
+$(EXE).tns: $(EXE).elf
+	$(GENZEHN) --input $^ --output $@.zehn $(ZEHNFLAGS)
+	make-prg $@.zehn $@
+	rm $@.zehn
 
 clean:
-	rm -f ${OBJS}
-	rm -f voxelspace
-
-.PHONY := all clean
+	rm -f $(OBJS) $(DISTDIR)/$(EXE).tns $(DISTDIR)/$(EXE).elf $(DISTDIR)/$(EXE).zehn
--- voxelspace.c
+++ voxelspace.c
@@ -1,25 +1,25 @@
+#include <libndls.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-#include <SDL2/SDL.h>
 
-#define FRAMETIME (16.666)
 #define CLAMP(x, a, b) ((x) > (b) ? (b) : (x) < (a) ? (a) : (x))
 
-SDL_Window *win;
-SDL_Renderer *renderer;
-int win_w;
-int win_h;
+#define RSCALE (4)
+#define S_WIDTH 320
+#define S_HEIGHT 240
+#define W_WIDTH (S_WIDTH/RSCALE)
+#define W_HEIGHT (S_HEIGHT/RSCALE)
 
-size_t mapsize;
+int mapsize;
 uint8_t *greyscale_bmp;
 uint8_t *level_bmp;
+uint16_t *framebuffer[2];
 
 typedef struct {
 	double x;
@@ -33,14 +33,13 @@ typedef struct {
 
 camera_t camera;
 
-double val = 240.0;
-
 static void die(const char *fmt, ...) {
 	va_list args;
 	va_start (args, fmt);
-	fprintf(stderr, "[OOPS] ");
-	vfprintf(stderr, fmt, args);
-	putc('\n', stderr);
+	char buf[512];
+	vsnprintf(buf, sizeof(buf), fmt, args);
+	lcd_init(SCR_TYPE_INVALID);
+	show_msgbox("OOPS", buf);
 	va_end(args);
 	exit(1);
 }
@@ -48,120 +47,133 @@ static void die(const char *fmt, ...) {
 void load_images(const char *basename, const char *ext) {
 	char filename[256];
 	int w, h, chans;
-	
-	snprintf(filename, sizeof(filename), "%s_bw.%s", basename, ext);
+
+	snprintf(filename, sizeof(filename), "%s_bw.%s.tns", basename, ext);
 	greyscale_bmp = stbi_load(filename, &w, &h, &chans, STBI_grey);
 	if (greyscale_bmp == NULL || w != h || chans != 1)
 		die("failed to load greyscale image './%s'", filename);
-	
+
 	int expected_size = w;
 
-	snprintf(filename, sizeof(filename), "%s_col.%s", basename, ext);
+	snprintf(filename, sizeof(filename), "%s_col.%s.tns", basename, ext);
 	level_bmp = stbi_load(filename, &w, &h, &chans, STBI_rgb);
 	if (level_bmp == NULL || w != expected_size || h != expected_size || chans != 3)
 		die("failed to load level image './%s'", filename);
-	
+
 	mapsize = w;
 }
 
 void init(void) {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-		die("failed to init sdl %s", SDL_GetError());
-
-	win = SDL_CreateWindow("VoxelSpace", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
-	if (win == NULL)
-		die("failed to create window: %s", SDL_GetError());
-	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_SOFTWARE);
-	if (renderer == NULL)
-		die("failed to create renderer: %s", SDL_GetError());
+	lcd_init(SCR_320x240_565);
+	framebuffer[0] = malloc(S_HEIGHT * S_WIDTH * sizeof(uint16_t));
+	framebuffer[1] = malloc(W_HEIGHT * W_WIDTH * sizeof(uint16_t));
+	memset(framebuffer[1], 0, W_HEIGHT * W_WIDTH * sizeof(uint16_t));
 
 	camera = (camera_t){
 		.x = 512.0,
 		.y = 512.0,
-		.height = 80.0,
+		.height = 40.0,
 		.yaw = 0.0,
-		.horizon = 100.0,
+		.horizon = 40.0,
 		.scale = 1.0,
-		.distance = 800,
+		.distance = 80,
 	};
 }
 
-static uint32_t sample(uint8_t *data, size_t size, uint8_t channels, int x, int y) {
+static uint32_t sample(uint8_t *data, int size, int channels, int x, int y) {
+	x /= 2;
+	y /= 2;
 	int idx = ((abs(y) % size) * size + (abs(x) % size)) * channels;
 	if (channels == 1)
 		return data[idx];
 	else if (channels == 3)
 		return data[idx + 0] << 16 | data[idx + 1] << 8 | data[idx + 2];
+	else
+		abort();
 }
 
-void update(void) {
-	// SDL_GetRendererOutputSize(renderer, &win_w, &win_h);
-	win_w = 800;
-	win_h = 600;
-	const uint8_t *key_states = SDL_GetKeyboardState(NULL);
-	
-	// View distance
-	camera.distance += key_states[SDL_SCANCODE_LEFTBRACKET] * -20 + key_states[SDL_SCANCODE_RIGHTBRACKET] * 20;
-	camera.distance = CLAMP(camera.distance, 40, 3000);
-	
+static void update(void) {
 	// Camera orientation
-	camera.yaw += (key_states[SDL_SCANCODE_LEFT] + -1 * key_states[SDL_SCANCODE_RIGHT]) * 0.04;
-	camera.yaw += (key_states[SDL_SCANCODE_A] + -1 * key_states[SDL_SCANCODE_D]) * 0.04;
-	camera.horizon += (key_states[SDL_SCANCODE_UP] + -1 * key_states[SDL_SCANCODE_DOWN]) * 10.0;
-	
+	camera.yaw += (isKeyPressed(KEY_NSPIRE_4) + -1 * isKeyPressed(KEY_NSPIRE_6)) * 0.04;
+	camera.horizon += (isKeyPressed(KEY_NSPIRE_PLUS) + -1 * isKeyPressed(KEY_NSPIRE_MINUS)) * 5.0;
+
 	// Camera position
-	double forward = key_states[SDL_SCANCODE_S] + -1 * key_states[SDL_SCANCODE_W];
-	camera.x += forward * sin(camera.yaw) * camera.scale;
-	camera.y += forward * cos(camera.yaw) * camera.scale;
-	
-	val += (key_states[SDL_SCANCODE_T] + -1 * key_states[SDL_SCANCODE_G]) * 1.0;
+	double forward = isKeyPressed(KEY_NSPIRE_5) + -1 * isKeyPressed(KEY_NSPIRE_8);
+	camera.x += forward * sin(camera.yaw) * camera.scale * 1.5;
+	camera.y += forward * cos(camera.yaw) * camera.scale * 1.5;
 
 	// Camera height
-	camera.height += (key_states[SDL_SCANCODE_E] + -1.0 * key_states[SDL_SCANCODE_Q]) * 2.0;
-	uint8_t height_sample = sample(greyscale_bmp, mapsize, 1, (int)camera.x, (int)camera.y) + 10;
+	camera.height += (isKeyPressed(KEY_NSPIRE_9) + -1.0 * isKeyPressed(KEY_NSPIRE_7)) * 0.8;
+	uint8_t height_sample = sample(greyscale_bmp, mapsize, 1, (int)camera.x, (int)camera.y) + 5;
 	if (height_sample > camera.height) // Collision with ground
 		camera.height = height_sample;
 }
 
-static void draw_column(int x, uint16_t ybot, uint16_t ytop, uint32_t col) {
+static void draw_column(uint32_t x, uint32_t ybot, uint32_t ytop, uint32_t col) {
 	if (ytop > ybot)
 		return;
-	SDL_SetRenderDrawColor(renderer, (col >> 16) & 0xFF, (col >> 8) & 0xFF, col & 0xFF, SDL_ALPHA_OPAQUE);
-	SDL_RenderDrawLine(renderer, x, ybot, x, CLAMP(ytop, 0, win_h));
+
+	uint16_t col4 = 0;
+	col4 |= ((col >> 19) & 0x1F) << 11;
+	col4 |= ((col >> 10) & 0x3F) <<  5;
+	col4 |= ((col >>  3) & 0x1F) <<  0;
+
+	ytop = ytop > W_HEIGHT ? W_HEIGHT : ytop;
+	for (; ytop < ybot; ++ytop)
+		framebuffer[1][ytop * W_WIDTH + x] = col4;
 }
 
-// Uses a disgusting amount of floating point math, ideally should use none
-// TODO: add LOD, render less often the further away
-void draw(void) {
+static void blit(void) {
+	// copy over and scale
+	for (uint32_t y = 0; y < W_HEIGHT; ++y) {
+		for (uint32_t x = 0; x < W_WIDTH; ++x) {
+			uint32_t xs = x * RSCALE;
+			uint32_t ys = y * RSCALE;
+			uint16_t col = framebuffer[1][y * W_WIDTH + x];
+			for (uint32_t yl = 0; yl < RSCALE; ++yl)
+				for (uint32_t xl = 0; xl < RSCALE; ++xl)
+					framebuffer[0][(ys + yl) * S_WIDTH + (xs + xl)] = col;
+		}
+	}
+
+	lcd_blit(framebuffer[0], SCR_320x240_565);
+	for (uint32_t i = 0; i < W_WIDTH * W_HEIGHT; ++i)
+		framebuffer[1][i] = 0x771F;
+}
+
+static void draw(void) {
 	// Y-buffer
-	uint16_t ybuf[win_w];
-	for (int i = 0; i < win_w; ++i)
-		ybuf[i] = win_h;
-	
+	uint16_t ybuf[W_WIDTH];
+	for (uint32_t i = 0; i < W_WIDTH; ++i)
+		ybuf[i] = W_HEIGHT;
+
 	// Camera angles
 	double sina = sin(camera.yaw);
 	double cosa = cos(camera.yaw);
-	
+
 	// Draw from front to back. use y buffer
-	for (int z = 1; z < camera.distance; z += 1) {
+	for (uint32_t z = 1; z < camera.distance * 4; z += 4) {
+		double zd = (double)z;
+
 		// Left and right ends of our sampling line
-		double splx = (-cosa*z - sina*z) * camera.scale;
-		double sply = ( sina*z - cosa*z) * camera.scale;
-		double sprx = ( cosa*z - sina*z) * camera.scale;
-		double spry = (-sina*z - cosa*z) * camera.scale;
-		
+		double splx = (-cosa*zd - sina*zd) * camera.scale;
+		double sply = ( sina*zd - cosa*zd) * camera.scale;
+		double sprx = ( cosa*zd - sina*zd) * camera.scale;
+		double spry = (-sina*zd - cosa*zd) * camera.scale;
+
 		// Segment the line into sample intervals
-		double dx = (sprx - splx) / (double)win_w;
-		double dy = (spry - sply) / (double)win_w;
+		double dx = (sprx - splx) / (double)W_WIDTH;
+		double dy = (spry - sply) / (double)W_WIDTH;
 		splx += camera.x;
 		sply += camera.y;
-		
+
 		// Draw the columns left to right, starting from the top of the highest pixel
-		for (int i = 0; i < win_w; ++i) {
-			uint8_t height_sample = sample(greyscale_bmp, mapsize, 1, (int)splx, (int)sply);
-			uint16_t height_screen = (camera.height - (double)height_sample) * (1.0 / (double)z * val) + camera.horizon;
+		double zs = (1.0 / zd * 240.0);
+		for (uint32_t i = 0; i < W_WIDTH; ++i) {
+			double height_sample = (double)sample(greyscale_bmp, mapsize, 1, (int)splx, (int)sply);
+			uint16_t height_screen = (camera.height - height_sample) * zs + camera.horizon;
 			uint32_t diffuse_sample = sample(level_bmp, mapsize, 3, (int)splx, (int)sply);
-			
+
 			draw_column(i, ybuf[i], height_screen, diffuse_sample);
 			if (height_screen < ybuf[i])
 				ybuf[i] = height_screen;
@@ -176,90 +188,32 @@ void cleanup(void) {
 	// Free level resources
 	free(greyscale_bmp);
 	free(level_bmp);
-
-	// Free SDL resources
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(win);
-	SDL_Quit();
+	for (uint32_t i = 0; i < 2; ++i)
+		free(framebuffer[i]);
+	lcd_init(SCR_TYPE_INVALID);
 }
 
-void print_usage(char *prog_name) {
-	fprintf(stderr,
-		"Usage: %s [OPTIONS]...\n"
-		"Render a height map with colour.\n\n", prog_name);
-	fprintf(stderr,
-		"Optional Arguments:\n"
-		"  -h             displays this prompt\n"
-		"  -l [level]     takes a level prefix,\n"
-		"                   e.g. -l 'res/level02'\n"
-		"  -e [extension] specifies an extension for the level\n"
-		"  -s [scale]     takes a number as a scale for size and movement\n\n");
-	fprintf(stderr,
-		"Source available at: <https://github.com/serxka/voxelspace>\n");
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
+	enable_relative_paths(argv);
+	(void)argc;
+	
 	// Process arguments
-	int c;
-	char *level = "res/level02";
-	char *ext = "png";
-	double scale = 1.0;
-	while ((c = getopt(argc, argv, "hs:l:e:")) != -1)
-		switch (c) {
-			case 'l':
-				level = optarg;
-				break;
-			case 's':
-				scale = atof(optarg);
-				break;
-			case 'e':
-				ext = optarg;
-				break;
-			case 'h':
-				print_usage(argv[0]);
-				exit(1);
-			case '?':
-				die("opt -%c requires an argument", optopt);
-		}
-		
-	// Setup SDL
-	init();
-	camera.scale = scale;
+	const char *level = "res/level01";
+	const char *ext = "png";
 
-	// Load resources, like level data
+	// Setup program
+	init();
 	load_images(level, ext);
 
+	// Main game loop
 	bool quit = false;
 	while (!quit) {
-		int start_tick = SDL_GetTicks();
-		// Poll for events
-		SDL_Event evt;
-		while (SDL_PollEvent(&evt) != 0)
-			switch (evt.type) {
-				case SDL_QUIT:
-					quit = true;
-					break;
-				default:
-					break;
-			}
-		// Update state
+		quit = isKeyPressed(KEY_NSPIRE_ESC);
 		update();
-		
-		// Render our level
-		SDL_SetRenderDrawColor(renderer, 135, 206, 235, SDL_ALPHA_OPAQUE);
-		SDL_RenderClear(renderer);
-		// clock_t start_time = clock();
 		draw();
-		// double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
-		// printf("drawing took %fms\n", elapsed_time * 1000.0);
-		SDL_RenderPresent(renderer);
-		
-		// Wait for next frame
-		int current_tick = SDL_GetTicks();
-		int period = FRAMETIME - (current_tick - start_tick);
-		period = period < 0 ? 0 : period;
-		SDL_Delay(period);
+		blit();
 	}
+
 	// Clean up resources
 	cleanup();
 	return 0;
